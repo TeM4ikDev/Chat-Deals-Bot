4ebb05071b79ae96054a50f4ec35e9f8e9052f59
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-09-26 13:10:34 +0200

    video download

--- backend/src/telegram/updates/businessMode.update.ts
+++ backend/src/telegram/updates/businessMode.update.ts
@@ -337,6 +337,7 @@ export class BusinessMessageUpdate {
                     const videoInfo: any = await youtubedl(msg.text, {
                         dumpSingleJson: true,
                         cookies: './cookies.txt',
+                        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                         // format: 'best',
                         skipDownload: true,
                         ignoreErrors: true,
