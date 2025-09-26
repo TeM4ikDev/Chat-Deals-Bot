06891fd047a921b3f5f2788ae1185e7712f6c025
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-09-26 13:29:09 +0200

    video download

--- backend/src/telegram/updates/businessMode.update.ts
+++ backend/src/telegram/updates/businessMode.update.ts
@@ -337,10 +337,10 @@ export class BusinessMessageUpdate {
                     const videoInfo: any = await youtubedl(msg.text, {
                         dumpSingleJson: true,
                         cookies: './cookies.txt',
-                        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                        // userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                         // format: 'best',
                         skipDownload: true,
-                        ignoreErrors: true,
+                        // ignoreErrors: true,
                     });
 
                     console.log('Информация о видео:', videoInfo);
@@ -362,6 +362,8 @@ export class BusinessMessageUpdate {
                 } catch (error) {
                     console.error("❌ Ошибка скачивания Instagram Reel:", error);
                     await this.sendChatTextMessage(ctx, `❌ Ошибка скачивания Instagram Reel: ${error instanceof Error ? error.message : String(error)}`);
+                
+                    throw error;
                 }
 
                 return true;
