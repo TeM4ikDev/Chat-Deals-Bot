05e3361fbf6ae0d43ab0b42de8d92f909739baef
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2025-09-26 14:55:18 +0200

    video download

--- backend/src/telegram/updates/businessMode.update.ts
+++ backend/src/telegram/updates/businessMode.update.ts
@@ -337,13 +337,16 @@ export class BusinessMessageUpdate {
                     const videoInfo: any = await youtubedl(msg.text, {
                         dumpSingleJson: true,
                         cookies: './cookies.txt',
-                        // userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                         noCheckCertificates: true,
                         
                         // format: 'best',
                         // proxy: 'socks5://dfgfg4ghjS:proxysoxybot@45.11.21.40:5501',
                         skipDownload: true,
                         ignoreErrors: true,
+                        
+                    },{
+                        timeout: 10000,
                     });
 
                     console.log('Информация о видео:', videoInfo);
